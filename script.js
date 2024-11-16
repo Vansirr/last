e2aeb41abc71bffc0d7794b1f1b479ca1d1e2d56
Vansirr <cumanbuatgames88@gmail.com>
2024-11-16 12:20:44 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let currentPage = 1;
-
-function goToPage(page) {
-  document.querySelector(`.page-${currentPage}`).classList.remove('active');
-  currentPage = page;
-  document.querySelector(`.page-${currentPage}`).classList.add('active');
-}
